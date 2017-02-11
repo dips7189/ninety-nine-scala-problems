b6475f2db1f827e3c961450699e2456bd06f63cb
dipen.lakhani <dipen.lakhani@ocado.com>
2017-02-11 22:04:53 +0000

    changing last to penultimate ... copy and waste

--- src/test/scala/challenges/P02Test.scala
+++ src/test/scala/challenges/P02Test.scala
@@ -8,18 +8,18 @@ class P02Test extends FlatSpec with Matchers {
     val emptyList = List()
 
     "Penultimate using another built-in" should "return saitō hajime" in {
-        val last = P02.penultimateUsingFoldLeft(l)
-        last should be ("saitō hajime")
+        val penultimate = P02.penultimateUsingFoldLeft(l)
+        penultimate should be ("saitō hajime")
     }
 
     "Penultimate using fold left" should "return saitō hajime" in {
-        val last = P02.penultimateUsingPatternMatching(l)
-        last should be ("saitō hajime")
+        val penultimate = P02.penultimateUsingPatternMatching(l)
+        penultimate should be ("saitō hajime")
     }
 
     "Penultimate using pattern matching" should "return saitō hajime" in {
-        val last = P02.penultimateUsingBuiltIn(l)
-        last should be ("saitō hajime")
+        val penultimate = P02.penultimateUsingBuiltIn(l)
+        penultimate should be ("saitō hajime")
     }
 
     "Penultimate using pattern matching, another built-in and using fold left" should "throw no such element exception" in {
