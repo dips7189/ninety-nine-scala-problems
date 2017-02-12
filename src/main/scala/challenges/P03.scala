452025501e1e2dd367144d1b95631e25f216c38a
dipen.lakhani <dipen.lakhani@ocado.com>
2017-02-12 11:24:00 +0000

    solutions to the third problem

---
+++ src/main/scala/challenges/P03.scala
@@ -0,0 +1,22 @@
+package challenges
+
+object P03 {
+
+    def nthUsingBuiltInLift[X](n: Int, l: List[X]): X = l.lift(n).get
+
+    def nthUsingBuiltInDrop[X](n: Int, l: List[X]): X = l.drop(n - 1).head
+
+    def nthUsingBuiltIn[X](n: Int, l: List[X]): Option[X] = if(n < 0 || n >= l.size) None else Some(l(n))
+
+    def nthUsingPatternMatching[X](n: Int, l: List[X]): X = l match {
+        case Nil => throw new NoSuchElementException
+        case x :: xs => if(n < 0) throw new NoSuchElementException else if(n == 0) x else nthUsingPatternMatching(n - 1, xs)
+    }
+
+    def nthUsingPatternMatchingAgain[X](n: Int, l: List[X]): X = (n, l) match {
+        case (0, x :: _) => x
+        case (k, _ :: xs) => nthUsingPatternMatching(k -1, xs)
+        case (_, Nil) => throw new NoSuchElementException
+    }
+
+}
---
+++ src/test/scala/challenges/P03Test.scala
@@ -0,0 +1,39 @@
+package challenges
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class P03Test extends FlatSpec with Matchers {
+
+    val l = List("Duo Maxwell", "Heero Yuy", "Quatre Raberba Winner", "Troa Barton", "Wufei Chang")
+    val emptyList = List()
+
+    "nth using built-in lift" should "return Heero Yuy" in {
+        val second = P03.nthUsingBuiltInLift(1, l)
+        second should be ("Heero Yuy")
+    }
+
+    "nth using built-in drop" should "return Duo Maxwell" in {
+        val first = P03.nthUsingBuiltInDrop(0, l)
+        first should be ("Duo Maxwell")
+    }
+
+    "nth using pattern matching" should "return Quatre Raberba Winner" in {
+        val third = P03.nthUsingPatternMatching(2, l)
+        third should be ("Quatre Raberba Winner")
+    }
+
+    "nth using pattern matching again" should "return Trao Barton" in {
+        val fourth = P03.nthUsingPatternMatchingAgain(3, l)
+        fourth should be ("Troa Barton")
+    }
+
+    "nth using built-in lift, built-in drop, and both pattern matching" should "throw no such element exception" in {
+        an [NoSuchElementException] should be thrownBy P03.nthUsingBuiltInLift(0, emptyList)
+        an [NoSuchElementException] should be thrownBy P03.nthUsingBuiltInDrop(3, emptyList)
+        an [NoSuchElementException] should be thrownBy P03.nthUsingPatternMatching(1, emptyList)
+        an [NoSuchElementException] should be thrownBy P03.nthUsingPatternMatchingAgain(2, emptyList)
+    }
+
+
+
+}
