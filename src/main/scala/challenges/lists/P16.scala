86a2eeded101e656a87fcc0f8076900d5ca0af46
dips7189 <lakhanid@hotmail.co.uk>
2017-02-22 18:16:14 +0000

    adding tailrec annotation

--- src/main/scala/challenges/lists/P16.scala
+++ src/main/scala/challenges/lists/P16.scala
@@ -1,8 +1,11 @@
 package challenges.lists
 
+import scala.annotation.tailrec
+
 object P16 {
 
     def drop[X](n: Int, l: List[X]): List[X] = {
+        @tailrec
         def _drop[X](i: Int, l: List[X], r: List[X]): List[X] = (i, l) match {
             case (1, _ :: xs) => _drop(n, xs, r)
             case (i, x :: xs) => _drop(i - 1, xs, x :: r)
